8c95c5da681d72eecd3d49f966d4ff7b4e2219b2
Alan Diniz <alanzinid@gmail.com>
2024-10-03 16:35:18 -0300

    feat: create department entity class

---
+++ src/aplication/Program.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department deparmtent = new Department(1, "alan");
+		
+		System.out.println(deparmtent);
+	}
+
+}
